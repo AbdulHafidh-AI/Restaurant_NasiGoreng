597230f25c06756bbea4a41ece68f6110a63d14f
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-04-30 00:26:31 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="assets/nasi-goreng.png" width="10%" alt=""> FRECE
+                    <img src="assets/nasi-goreng.png" width="10%" alt=""> <b>FRECE</b>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
