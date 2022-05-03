a767126d8544c519125228fc99871eec53aae2ed
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-05-03 14:28:34 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -223,13 +223,15 @@
                         </div>
                     </div>
                 </section>
-                <!-- Footer-->
-                <footer class="py-5 bg-dark">
-                    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Restaurant Nasi Goreng</p></div>
-                </footer>
+               
             </div>
         </div>
     </div>
+ <!-- Footer-->
+ <footer class="py-5 bg-dark">
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Restaurant Nasi Goreng</p></div>
+</footer>
+
     <!-- Back to top-->
     <div class="btn-back-to-top bg0-hov" id="myBtn"><span class="symbol-btn-back-to-top">
         <i class="fa fa-angle-double-up" aria-hidden="true"></i></span></div>
